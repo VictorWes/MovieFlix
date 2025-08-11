da8ae06f72e580260d9c0b4d2205ab94d93c49e1
Victor Wesley <victor.infowb@gmail.com>
2025-08-11 19:10:56 -0300

    fix: Corrige data releaseDate, para modelo brasileiro dd/MM/yyyy

--- src/main/java/br/com/movieflix/MovieFlix/Controller/request/MovieRequest.java
+++ src/main/java/br/com/movieflix/MovieFlix/Controller/request/MovieRequest.java
@@ -1,5 +1,6 @@
 package br.com.movieflix.MovieFlix.Controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -8,7 +9,10 @@ import java.util.List;
 @Builder
 public record MovieRequest(String title,
                            String description,
+
+                           @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                            LocalDate releaseDate,
+
                            double rating,
                            List<Long> categories,
                            List<Long> streamings){
