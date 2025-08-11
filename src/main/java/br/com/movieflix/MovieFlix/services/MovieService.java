53d42b0031cd1f2ad4a7e8e6ec59b3cdf63bad7c
Victor Wesley <victor.infowb@gmail.com>
2025-08-11 19:02:02 -0300

    fix: Implementa a correção em MovieService, para que em MovieController, utilizando a rota Post para Movie.Foi realizada a correção do retorno para aparecer em quais streamings o Movie se encontra

--- src/main/java/br/com/movieflix/MovieFlix/services/MovieService.java
+++ src/main/java/br/com/movieflix/MovieFlix/services/MovieService.java
@@ -1,25 +1,48 @@
 package br.com.movieflix.MovieFlix.services;
 
+import br.com.movieflix.MovieFlix.entity.Category;
 import br.com.movieflix.MovieFlix.entity.Movie;
+import br.com.movieflix.MovieFlix.entity.Streaming;
 import br.com.movieflix.MovieFlix.repository.MovieRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MovieService {
 
     private final MovieRepository  movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
-    public MovieService(MovieRepository movieRepository) {
+    public MovieService(MovieRepository movieRepository, CategoryService categoryService, StreamingService streamingService) {
         this.movieRepository = movieRepository;
+        this.categoryService = categoryService;
+        this.streamingService = streamingService;
     }
 
     public Movie save(Movie movie){
+        movie.setCategories(this.findCategories(movie.getCategories()));
+        movie.setStreamings(this.findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll(){
         return movieRepository.findAll();
+    }
+
+    private List<Category> findCategories(List<Category> categories){
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category ->
+                categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    private List<Streaming> findStreamings(List<Streaming> streamings){
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming ->
+                streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 }
